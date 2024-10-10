0400978b31177d710ac8ccc94c629275b1d963f5
ELJRAOUYOMAR <etudecompte78@gmail.com>
2024-10-10 15:09:29 -0700

    -

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" docs """
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """ sum of the floats in the list """
+    return sum(input_list)
