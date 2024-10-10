e0f5b56cd2919bc8bf421ac8c696e88ef137bd8b
ELJRAOUYOMAR <etudecompte78@gmail.com>
2024-10-10 15:15:58 -0700

    -

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" docs """
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """ sum of the mixed list(integers & floats) """
+    return sum(mxd_lst)
